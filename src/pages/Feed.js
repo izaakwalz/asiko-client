f21859c6d11e8638af4ca054201f0a3875c8aa3b
Ifenna-Pascal <71331395+Ifenna-Pascal@users.noreply.github.com>
2022-02-28 19:46:31 +0100

    set up the latest style of tailwindcss

--- src/pages/Feed.js
+++ src/pages/Feed.js
@@ -3,7 +3,7 @@ import React from 'react';
 const Feed = () => {
 	return (
 		<div>
-			<h3 className=' font-serif font-semibold'>Home Page</h3>
+			<h3 className=' font-serif font-semibold text-green-600'>Home Page</h3>
 			<a href='/' className=' mt-2 text-base'>
 				Home
 			</a>
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -3,7 +3,7 @@ import React from 'react';
 const Home = () => {
 	return (
 		<div>
-			<h3 className=' font-serif font-semibold'>Home Page</h3>
+			<h3 className=' font-serif text-red-400 font-semibold'>Home Page</h3>
 			<a href='/user' className=' mt-2 text-base'>
 				Feed
 			</a>
